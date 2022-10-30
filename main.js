725d635635fa0f32ca647d5875a3ecb2d0e16205
Nadun Gunawardana <nadungn@gmail.com>
2022-10-31 01:07:05 +0530

    31 Oct 2022 - Image Carousels Updated, Refactored

--- main.js
+++ main.js
@@ -1,40 +1,43 @@
+// * Number of Photos in each collection in chronological order
+const works = {
+  1: 5, // ? Shanu Pradeep & Sandya Kumari Keshayur Salon Promoting Shoot
+  2: 7, // ? Jinuli Onaya Birthday
+  3: 6, // ? Dilini Bridal Frock
+  4: 8, // ? Dilini  Bridal Saree
+  5: 11, // ? Chandi & Mahesh Engagement
+  6: 15, // ? Dilini Fashion Frock
+  7: 14, // ? Dilini Fashion Saree
+};
 
-// init Masonry
-
+const services = {
+  1: 3, // ? Wedding Photography
+  2: 2, // ? Wedding Preshoot
+};
 
 let index = 0;
-const totalWorkItems = $(".work-item").length;
-let totalServiceItems = 0;
-let isService = true
-let serveIndex = 0;
-let position = 0
-const services = {
-    weddingphoto: 3,
-    preshoot: 2
-}
+let totalItems = 0;
+let isService = true;
+let position = 0;
 
-$(window).on("load", function () {
-    $(".preloader").addClass("loaded");
-})
+$(window).on('load', function () {
+  $('.preloader').addClass('loaded');
+});
 
 // init Masonry
 var $grid = $('.work-row').masonry({
-    itemSelector: '.work-item',
-    percentPosition: true,
-    columnWidth: '.work-sizer'
+  itemSelector: '.work-item',
+  percentPosition: true,
+  columnWidth: '.work-sizer',
 });
 
 // layout Masonry after each image loads
 $grid.imagesLoaded().progress(function () {
-    $grid.masonry();
+  $grid.masonry();
 });
-
-
-
 
 // $(window).resize(function () {
 //     var $container = $('#gallery');
-//     // initialize Masonry after all images have loaded  
+//     initialize Masonry after all images have loaded
 //     $container.imagesLoaded(function () {
 //         $container.masonry({
 //             columnWidth: ".work-item",
@@ -45,201 +48,155 @@ $grid.imagesLoaded().progress(function () {
 // });
 
 $(document).ready(function () {
-    // var $container = $('#gallery');
-    // // initialize Masonry after all images have loaded  
-    // $container.imagesLoaded(function () {
-    //     $container.masonry({
-    //         columnWidth: ".work-item",
-    //         itemSelector: ".work-item",
-    //         percentPosition: true
-    //     });
-    // });
+  // var $container = $('#gallery');
+  // // initialize Masonry after all images have loaded
+  // $container.imagesLoaded(function () {
+  //     $container.masonry({
+  //         columnWidth: ".work-item",
+  //         itemSelector: ".work-item",
+  //         percentPosition: true
+  //     });
+  // });
 
+  // // Refresh Orientation
+  // $(window).on('orientationchange', function (e) {
+  //     $.mobile.changePage(window.location.href, {
+  //         allowSamePageTransition: true,
+  //         transition: 'none',
+  //         reloadPage: true
+  //     });
+  // });
+  // nav toggle
+  $('.nav-toggle').click(function () {
+    $('.header .nav').slideToggle();
+  });
+  $('.header .nav a').click(function () {
+    if ($(window).width() < 768) {
+      $('.header .nav').slideToggle();
+    }
+  });
 
-    // // Refresh Orientation
-    // $(window).on('orientationchange', function (e) {
-    //     $.mobile.changePage(window.location.href, {
-    //         allowSamePageTransition: true,
-    //         transition: 'none',
-    //         reloadPage: true
-    //     });
-    // });
+  //fixed header
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.header').addClass('fixed');
+    } else {
+      $('.header').removeClass('fixed');
+    }
+  });
 
-    // nav toggle
-    $(".nav-toggle").click(function () {
-        $(".header .nav").slideToggle();
-    })
-    $(".header .nav a").click(function () {
-        if ($(window).width() < 768) {
-            $(".header .nav").slideToggle();
+  // Add smooth scrolling to all links
+  $('a').on('click', function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== '') {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
         }
-    })
+      );
+    } // End if
+  });
 
-    //fixed header
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $(".header").addClass("fixed");
-        }
-        else {
-            $(".header").removeClass("fixed");
-        }
-    })
+  // $('#gallery').masonry({
+  //     // set itemSelector so .grid-sizer is not used in layout
+  //     itemSelector: '.work-item',
+  //     columnWidth: '.work-item',
+  // })
 
-    // Add smooth scrolling to all links
-    $("a").on('click', function (event) {
+  // set lightbox img max height
+  const wHeight = $(window).height();
+  $('.lightbox-img').css('max-height', wHeight + 'px');
+  // lightbox
+  $('.work-item-inner').click(function () {
+    isService = false;
+    selectSection('work', this);
+  });
+  $('.service-item-inner').click(function () {
+    isService = true;
+    selectSection('service', this);
+  });
 
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
+  function getTotalItems(section, position) {
+    if (section == 'work') {
+      return works[position.toString()];
+    } else if (section == 'service') {
+      return services[(position + 1).toString()];
+    }
+  }
 
-            // Store hash
-            var hash = this.hash;
+  function selectSection(section, place) {
+    position = $(place).parent(`.${section}-item`).index();
+    $('.lightbox').addClass('open');
+    index = 1;
+    totalItems = getTotalItems(section, position);
+    lightboxSlideShow(section);
+  }
+  $('.lightbox .prev').click(function () {
+    if (index == 1) {
+      index = totalItems;
+    } else {
+      index--;
+    }
+    isService ? lightboxSlideShow('service') : lightboxSlideShow('work');
+  });
 
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function () {
+  $('.lightbox .next').click(function () {
+    if (index == totalItems) {
+      index = 1;
+    } else {
+      index++;
+    }
+    isService ? lightboxSlideShow('service') : lightboxSlideShow('work');
+  });
 
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-            });
-        } // End if
-    });
-
-    // $('#gallery').masonry({
-    //     // set itemSelector so .grid-sizer is not used in layout
-    //     itemSelector: '.work-item',
-    //     columnWidth: '.work-item',
-    // })
-
-    // set lightbox img max height
-    const wHeight = $(window).height();
-    $(".lightbox-img").css("max-height", wHeight + "px");
-
-    // lightbox
-    $(".work-item-inner").click(function () {
-        console.log(this);
-        index = $(this).parent(".work-item").index() - 1;
-        console.log(index);
-        $(".lightbox").addClass("open");
-        isService = false
-        lightboxSlideShow("work");
-    })
-    $(".service-item-inner").click(function () {
-        position = $(this).parent(".service-item").index();
-        console.log(position);
-        $(".lightbox").addClass("open");
-        isService = true
-        serveIndex = 0;
-        switch (position) {
-            case 0:
-                totalServiceItems = services.weddingphoto;
-                break;
-            case 1:
-                totalServiceItems = services.preshoot;
-                break;
-        }
-        lightboxSlideShow("service");
-
-    })
-    $(".lightbox .prev").click(function () {
-        if (isService) {
-            switch (position) {
-                case 0:
-                    totalServiceItems = services.weddingphoto;
-                    break;
-                case 1:
-                    totalServiceItems = services.preshoot;
-                    break;
-            }
-            if (serveIndex == 0) {
-                serveIndex = totalServiceItems - 1;
-            } else {
-                serveIndex--;
-            }
-            lightboxSlideShow("service");
-
-        }
-        if (!isService) {
-            if (index == 0) {
-                index = totalWorkItems - 1;
-            } else {
-                index--;
-            }
-            lightboxSlideShow("work");
-        }
-
-    })
-    $(".lightbox .next").click(function () {
-        if (isService) {
-            switch (position) {
-                case 0:
-                    totalServiceItems = services.weddingphoto;
-                    break;
-                case 1:
-                    totalServiceItems = services.preshoot;
-                    break;
-            }
-            if (serveIndex == totalServiceItems - 1) {
-                serveIndex = 0;
-            } else {
-                serveIndex++;
-            }
-            lightboxSlideShow("service");
-
-        }
-        if (!isService) {
-            if (index == totalWorkItems - 1) {
-                index = 0
-            } else {
-                index++;
-            }
-            lightboxSlideShow("work");
-        }
-    })
-
-    // close lightbox
-    $(".lightbox-close").click(function () {
-        $(".lightbox").removeClass("open");
-    })
-    // close lightbox when clicked outside of img-box
-    $(".lightbox").click(function (event) {
-        if ($(event.target).hasClass("lightbox")) {
-            $(this).removeClass("open");
-        }
-    })
-})
+  // close lightbox
+  $('.lightbox-close').click(function () {
+    $('.lightbox').removeClass('open');
+  });
+  // close lightbox when clicked outside of img-box
+  $('.lightbox').click(function (event) {
+    if ($(event.target).hasClass('lightbox')) {
+      $(this).removeClass('open');
+    }
+  });
+});
 
 function lightboxSlideShow(section) {
+  let category = '';
+  const imgSrc = getDir(section) + `${index}.jpg`;
+  if (section == 'work') {
+    category = $(`.${section}-item`)
+      .eq(position - 1)
+      .find('h4')
+      .html();
+  } else if (section == 'service') {
+    category = $(`.${section}-item`).eq(position).find('h4').html();
+  }
+  $('.lightbox-counter').html(index + '/' + totalItems);
+  $('.lightbox-img').attr('src', imgSrc);
+  $('.lightbox-category').html(category);
+}
 
-    let imgSrc = ""
-    let category = ""
-    if (section == "work") {
-        imgSrc = $(".work-item").eq(index).find("img").attr("data-large");
-        category = $(".work-item").eq(index).find("h4").html();
-        $(".lightbox-counter").html((index + 1) + "/" + totalWorkItems);
-    }
-    else if (section == "service") {
-        const serviceDir = "./img/Services Section/large"
-        switch (position) {
-            case 0:
-                imgSrc = serviceDir + `/wedding-photo/${serveIndex}.jpg`
-                break;
-            case 1:
-                imgSrc = serviceDir + `/pre-shoot/${serveIndex}.jpg`
-                break;
-        }
-        category = $(".service-item").eq(position).find("h4").html();
-        $(".lightbox-counter").html((serveIndex + 1) + "/" + totalServiceItems);
-    }
-
-
-    $(".lightbox-img").attr("src", imgSrc);
-    $(".lightbox-category").html(category);
-
-
-
-
+function getDir(section) {
+  if (section == 'work') {
+    return $(`.${section}-item`)
+      .eq(position - 1)
+      .find('img')
+      .attr('data-large');
+  } else if (section == 'service') {
+    return $(`.${section}-item`).eq(position).find('img').attr('data-large');
+  }
 }
